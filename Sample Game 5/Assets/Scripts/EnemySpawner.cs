b1962683f5732c4b5eb533074be6553d6247635a
Sayanabha Chandra <sayanabhachandraitsme@gmail.com>
2023-03-25 23:39:20 +0530

    1st Commit

---
+++ Sample Game 5/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+
+    public float xPositionLimit;
+    public float spawnRate;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //SpawnSpike();
+        StartSpawning();
+    }
+
+
+    void SpawnSpike()
+    {
+        float randomX = Random.Range(-xPositionLimit, xPositionLimit);
+
+        Vector2 spawnPosition = new Vector2(randomX, transform.position.y);
+
+        Instantiate(enemy, spawnPosition, Quaternion.identity);
+    }
+
+
+    void StartSpawning()
+    {
+        InvokeRepeating("SpawnSpike", 1f, spawnRate);
+    }
+
+    public void StopSpawning()
+    {
+        CancelInvoke("SpawnSpike");
+    }
+}
